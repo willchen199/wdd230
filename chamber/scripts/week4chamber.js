fdb5b048a6f1ac0f6b302ac17457b682ed235f47
Will Chen <williamexs@byui.edu>
2023-02-10 18:16:10 -0700

    saedf

--- chamber/scripts/week4chamber.js
+++ chamber/scripts/week4chamber.js
@@ -15,3 +15,12 @@ const hambutton = document.querySelector('.ham');
 hambutton.addEventListener("click", ()=>{
     mainnav.classList.toggle('responsive');
 })
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').innerHTML = '&copy;'+ year;
+
+let currentdate = document.lastModified;
+
+document.querySelector('.Update').textContent = `Last Updated: ${currentdate}`;
