b0101afb66da07b0914107c1f34740ce61e5131d
Will Chen <williamexs@byui.edu>
2023-03-01 13:53:20 -0700

    sadga

--- chamber/scripts/week4chamber.js
+++ chamber/scripts/week4chamber.js
@@ -19,12 +19,26 @@ let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
 day = days[day];
 let dayofmonth = date.getDate();
 let month =date.getMonth();
+let months =["January","February","March","April", "May","June","July","August","September","October","November","December"];
+month = months[month];
 
-document.querySelector('#date').innerHTML = day+"    " +dayofmonth+month+year
+document.querySelector('#date').innerHTML = `${day}, ${month} ${dayofmonth} ${year}`
 
 let string1 = "Trevor";
 let string2 = "Will";
 let formattedString = `${string1} and ${string2} are best buddies.`;
+
+async function weatherApi (){
+    await fetch("https://api.openweathermap.org/data/2.5/weather?lat=22.326&lon=114.164&appid=d10fd5d0017bbcf8e6a91168cde9ae7c&units=metric")
+    .then(response => response.json())
+    .then(data => {
+        data.coord.lon;
+        data.weather.id;
+        data.main.temp;
+        data.wind.deg;
+    })
+    
+}
 
 
 
--- lesson09/prophets.js
+++ lesson09/prophets.js
@@ -5,7 +5,7 @@ async function getProphetData() {
     console.log(response);
     const data = await response.json();
     console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayProphets(data.prophets);
+    displayTable(data.prophets);
 }
     
   getProphetData();
@@ -36,4 +36,35 @@ async function getProphetData() {
       cards.appendChild(card);
     } // end of forEach loop
   )} // end of function expression
+
+  function displayTable(prophets){
+
+    let row = document.querySelectorAll('tr');
+        row.forEach((item)=>{
+          item.remove();
+        })
+
+    let section = document.querySelectorAll('section');
+        section.forEach((item)=> {
+          item.remove();
+        });
+    
+
+    prophets.forEach((prophet)=> {
+      let tr = document.createElement('tr');
+      let td_name = document.createElement('td');
+      let td_birthplace = document.createElement('td');
+      let td_birthdate = document.createElement('td');
+
+      td_name.textContent = `${prophet.name} ${prophet.lastname}`;
+      td_birthplace.textContent = prophet.birthplace
+      td_birthdate.textContent = prophet.birthdate;
+      
+      tr.appendChild(td_name);
+      tr.appendChild(td_birthdate);
+      tr.appendChild(td_birthplace);
+
+      document.querySelector('table').appendChild(tr);
+    })
+  }
 
