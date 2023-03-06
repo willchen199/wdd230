f6c3bb06d6238d2bf1f278d7fef73f205c83ae47
Will Chen <williamexs@byui.edu>
2023-03-06 13:20:07 -0700

    jhhh

--- chamber/scripts/week4chamber.js
+++ chamber/scripts/week4chamber.js
@@ -32,13 +32,48 @@ async function weatherApi (){
     await fetch("https://api.openweathermap.org/data/2.5/weather?lat=22.326&lon=114.164&appid=d10fd5d0017bbcf8e6a91168cde9ae7c&units=metric")
     .then(response => response.json())
     .then(data => {
-        data.coord.lon;
-        data.weather.id;
-        data.main.temp;
-        data.wind.deg;
+        //Get temp
+        let temp = data.main.temp;
+        temp = (temp * (9/5)) + 32;
+        temp= Math.round(temp);
+        console.log(temp);
+        document.querySelector('.temp').innerHTML = `${temp} °F`;
+
+        // Get windspeed
+        let windspeed = data.wind.speed;
+        console.log(windspeed);
+        document.querySelector('.windspeed').innerHTML=`${windspeed} km/h`;
+
+        //Get windchill
+        let windchill = data.main.feels_like;
+        windchill = (windchill * (9/5)) + 32;
+        windchill = Math.round(windchill);
+        document.querySelector('.windchill').innerHTML=`${windchill} °F`;
+
+        // Get description
+        let description = data.weather[0].description;
+        document.querySelector('.description').innerHTML= description;
+
+        // Get weather icon
+        const weatherImg  = document.querySelector('.weathersign');
+        const icon        = data.weather[0].icon.toString();
+        (async () => {
+            const imageUrl = 'http://openweathermap.org/img/wn/' + icon + '@4x.png';
+            const response = await fetch(imageUrl);
+            const imageBlob = await response.blob();
+            const reader = new FileReader();
+            reader.readAsDataURL(imageBlob);
+            reader.onloadend = () => {
+                const base64data = reader.result;
+                weatherImg.src = base64data;
+            };
+
+        })();
+
     })
     
-}
+} weatherApi();
+
 
 
 
