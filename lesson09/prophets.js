e846c3807147b69caf1c4e78450f1474840a5781
Will Chen <williamexs@byui.edu>
2023-04-03 05:21:47 -0600

    dsfg

--- lesson09/prophets.js
+++ lesson09/prophets.js
@@ -4,11 +4,12 @@ async function getProphetData() {
     const response = await fetch(url);
     console.log(response);
     const data = await response.json();
-    console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayTable(data.prophets);
-}
-    
-  getProphetData();
+    console.table(data.prophets);  
+    displayProphets(data.prophets);
+  }
+  
+  getProphetData(); 
+
 
   const displayProphets = (prophets) => {
     const cards = document.querySelector('div.cards'); // select the output container element
@@ -20,15 +21,12 @@ async function getProphetData() {
       let portrait = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${prophet.name} ${prophet.lastname}`;
+      h2.textContent = `${prophet.name} ${prophet.lastname}` ;
   
       // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', prophet.imageurl);
       portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
-  
+
       // Append the section(card) with the created elements
       card.appendChild(h2);
       card.appendChild(portrait);
@@ -37,34 +35,9 @@ async function getProphetData() {
     } // end of forEach loop
   )} // end of function expression
 
-  function displayTable(prophets){
 
-    let row = document.querySelectorAll('tr');
-        row.forEach((item)=>{
-          item.remove();
-        })
 
-    let section = document.querySelectorAll('section');
-        section.forEach((item)=> {
-          item.remove();
-        });
-    
+   
 
-    prophets.forEach((prophet)=> {
-      let tr = document.createElement('tr');
-      let td_name = document.createElement('td');
-      let td_birthplace = document.createElement('td');
-      let td_birthdate = document.createElement('td');
 
-      td_name.textContent = `${prophet.name} ${prophet.lastname}`;
-      td_birthplace.textContent = prophet.birthplace
-      td_birthdate.textContent = prophet.birthdate;
-      
-      tr.appendChild(td_name);
-      tr.appendChild(td_birthdate);
-      tr.appendChild(td_birthplace);
-
-      document.querySelector('table').appendChild(tr);
-    })
-  }
 
