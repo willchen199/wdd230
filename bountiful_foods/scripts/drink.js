9541898fd890e6d020b3cd5f9d98b759d30a4ce0
Will Chen <williamexs@byui.edu>
2023-04-05 12:51:12 -0600

    sadf

--- bountiful_foods/scripts/drink.js
+++ bountiful_foods/scripts/drink.js
@@ -1,6 +1,8 @@
 // if (!localStorage.getItem('numDrinks')){
 //     localStorage.setItem('numDrinks', 0);
 //   }
+ 
+console.log(localStorage.getItem('numDrinks'));
   
   
-  document.querySelector("#numDrinks").innerHTML = localStorage.getItem('numDrinks');
+document.querySelector("#numDrinks").textContent = localStorage.getItem('numDrinks');
